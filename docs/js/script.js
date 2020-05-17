439a9f7a394920346afccb954902729e41fdd848
Briws <54640696+brightwiths@users.noreply.github.com>
2020-05-17 06:16:20 +0530

    first

---
+++ docs/js/script.js
@@ -0,0 +1,23 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById("myTopnav");
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else{
+		x.className = "topnav";
+	}
+}
+
+$(function(){
+	$('.toggles button').click(function(){
+		var get_id = this.id;
+		var get_current = $('.posts .' + get_id);
+
+		$('.post').not(get_current).hide(500);
+		get_current.show(500);
+	});
+
+	$('#showall').click(function(){
+		$('.post').show(500);
+	});
+});
